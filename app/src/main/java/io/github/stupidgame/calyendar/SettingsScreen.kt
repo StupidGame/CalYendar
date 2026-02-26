0803faf16847241d4276b8b7743909a87e774ea0
StarKidsYabakune <takashima.yamato@marble-corp.co.jp>
2026-02-26 11:30:09 +0900

    Webcalインポート時にもtoast表示が出るように変更

--- app/src/main/java/io/github/stupidgame/calyendar/SettingsScreen.kt
+++ app/src/main/java/io/github/stupidgame/calyendar/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package io.github.stupidgame.calyendar
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import io.github.stupidgame.calyendar.data.CalendarViewModel
 
@@ -29,6 +31,7 @@ fun SettingsScreen(
     calendarViewModel: CalendarViewModel,
     onImportIcsClick: (Boolean) -> Unit
 ) {
+    val context = LocalContext.current
     var webCalUrl by remember { mutableStateOf("") }
     var importAsHoliday by remember { mutableStateOf(false) }
     var notificationOneDayBefore by remember { mutableStateOf(true) }
@@ -70,7 +73,11 @@ fun SettingsScreen(
             )
             Spacer(modifier = Modifier.height(8.dp))
             Button(
-                onClick = { calendarViewModel.importWebcal(webCalUrl, importAsHoliday) {} },
+                onClick = {
+                    calendarViewModel.importWebcal(webCalUrl, importAsHoliday) { message ->
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                    }
+                },
                 modifier = Modifier.fillMaxWidth(),
                 enabled = webCalUrl.isNotBlank()
             ) {
